507475a94ab87ce4d82b8f8318cebe4cb397271d
Kooreis <kooreis.official@gmail.com>
2022-09-03 08:00:00 -0500

    Initial commit

---
+++ Countdown_Timer.cs
@@ -0,0 +1,29 @@
+```CSharp
+using System;
+using System.Threading;
+
+namespace CountdownTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the countdown time in seconds: ");
+            int countTime = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            while (countTime >= 0)
+            {
+                Console.SetCursorPosition(0, 0);
+                TimeSpan time = TimeSpan.FromSeconds(countTime);
+                Console.Write("Time Remaining: " + time.ToString(@"hh\:mm\:ss"));
+                Thread.Sleep(1000);
+                countTime--;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Countdown Complete!");
+        }
+    }
+}
+```
